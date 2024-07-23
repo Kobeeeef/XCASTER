cef323b2dc2c5b04e15979e925fc125c7f147185
kobel <kobelei335@gmail.com>
2024-07-23 00:11:45 -0700

    hostname property

--- src/main/java/org/kobe/xbot/Main.java
+++ src/main/java/org/kobe/xbot/Main.java
@@ -32,7 +32,7 @@ public class Main {
             InetAddress addr = Utilities.getLocalInetAddress();
             previousAddress = addr;
             jmdns = JmDNS.create(addr, hostname);
-            ServiceInfo serviceInfo = ServiceInfo.create("_xcaster._tcp.local.", "XCASTER - Service Broadcaster", 54321, "XCASTER by XBOT Robotics: Broadcasts hostname over the network.");
+            ServiceInfo serviceInfo = ServiceInfo.create("_xcaster._tcp.local.", "XCASTER - Service Broadcaster", 54321, "hostname=" + hostname);
             jmdns.registerService(serviceInfo);
 
             logger.info("mDNS service registered successfully.");
